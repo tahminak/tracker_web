c8fb67a0f9665fea65b16c8e3940e7fbabf2c034
Tahmina Khan <tahmina.khan@gmail.com>
2015-03-29 16:25:26 -0400

    Add $httpbackend test to test notesTabController

--- app/test/notesTabControllerSpec.js
+++ app/test/notesTabControllerSpec.js
@@ -15,29 +15,51 @@
         beforeEach(module('trackerApp'));
 
         var ctrl, scope;
+        var $httpBackend, createController, authRequestHandler;
 
         // inject the $controller and $rootScope services
         // in the beforeEach block\
-       
-       beforeEach(inject(function($controller,$rootScope){
-           //Create a new scope that's a child of the $rootScope
-           scope=$rootScope.$new();
-           
-           //Create the controller
-           ctrl=$controller('NotesTabController',{$scope:scope});
-           
-       }));
-       
-       it('Should define $scope.notesTabText',
-                function (){
+
+        beforeEach(inject(function ($controller, $rootScope, $injector) {
+            // Set up the mock http service responses
+            $httpBackend = $injector.get('$httpBackend');
+
+            // backend definition common for all tests
+            authRequestHandler = $httpBackend.when('GET', 'exampleNotes')
+                    .respond();
+
+
+            //Create a new scope that's a child of the $rootScope
+            scope = $rootScope.$new();
+
+            //Create the controller
+            ctrl = $controller('NotesTabController', {$scope: scope});
+
+        }));
+
+        it('Should define $scope.notesTabText',
+                function () {
                     expect(scope.notesTabText).toBeDefined();
                     scope.clearNotesTab();
                     expect(scope.notesTabText).toEqual("");
-                    
+                    $httpBackend.flush();
+
                 }
-        
+
         );
 
+
+        it('should fetch notes.json', function () {
+            $httpBackend.expectGET('exampleNotes');
+            var controller = ctrl;
+            $httpBackend.flush();
+        });
+
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        })
 
 
     });
