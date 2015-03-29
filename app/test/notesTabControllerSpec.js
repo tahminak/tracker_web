72900fc7ac21b7dab8d66dba949f193a0bfde1f5
Tahmina Khan <tahmina.khan@gmail.com>
2015-03-29 16:48:03 -0400

    Add $httpbackend test to test notesTabController

--- app/test/notesTabControllerSpec.js
+++ app/test/notesTabControllerSpec.js
@@ -20,20 +20,20 @@
         // inject the $controller and $rootScope services
         // in the beforeEach block\
 
-        beforeEach(inject(function ($controller, $rootScope, $injector) {
+        beforeEach(inject(function ($controller, $rootScope, $injector,$http) {
             // Set up the mock http service responses
             $httpBackend = $injector.get('$httpBackend');
 
             // backend definition common for all tests
             authRequestHandler = $httpBackend.when('GET', 'exampleNotes')
-                    .respond();
+                    .respond([{}]);
 
 
             //Create a new scope that's a child of the $rootScope
             scope = $rootScope.$new();
 
             //Create the controller
-            ctrl = $controller('NotesTabController', {$scope: scope});
+            ctrl = $controller('NotesTabController', {$scope: scope,$http: $http});
 
         }));
 
@@ -55,6 +55,10 @@
             $httpBackend.flush();
         });
 
+it("should have 1 object", function () {
+            $httpBackend.flush();
+            expect(scope.notesTabText.length).toBe(1);
+        });
 
         afterEach(function () {
             $httpBackend.verifyNoOutstandingExpectation();
